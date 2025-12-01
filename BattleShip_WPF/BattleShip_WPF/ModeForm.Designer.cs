09a7ffb753ea48aee80a6c5bab9b2d39618e5f45
zvshkin <n.kopnin1337@mail.ru>
2025-12-01 20:09:33 +0300

    UPD: Fix Font in Form

--- BattleShip_WPF/BattleShip_WPF/ModeForm.Designer.cs
+++ BattleShip_WPF/BattleShip_WPF/ModeForm.Designer.cs
@@ -31,12 +31,13 @@ namespace BattleShip_WPF
         {
             this.label1 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.CreateButton = new System.Windows.Forms.Button();
+            this.FastButton = new System.Windows.Forms.Button();
+            this.ClassicButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.BackButton = new System.Windows.Forms.Button();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -44,7 +45,7 @@ namespace BattleShip_WPF
             // 
             this.label1.BackColor = System.Drawing.Color.CornflowerBlue;
             this.label1.Dock = System.Windows.Forms.DockStyle.Top;
-            this.label1.Font = new System.Drawing.Font("Oi", 36F);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F);
             this.label1.ForeColor = System.Drawing.Color.Navy;
             this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
@@ -58,102 +59,121 @@ namespace BattleShip_WPF
             this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.BackButton, 0, 3);
             this.tableLayoutPanel1.Controls.Add(this.label4, 1, 2);
             this.tableLayoutPanel1.Controls.Add(this.label3, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this.button3, 0, 2);
-            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.button2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.CreateButton, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.FastButton, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.ClassicButton, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.label2, 1, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 111);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 3;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(984, 450);
             this.tableLayoutPanel1.TabIndex = 2;
             // 
-            // button1
+            // label4
             // 
-            this.button1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button1.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.Navy;
-            this.button1.FlatAppearance.BorderSize = 2;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Rubik Mono One", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.ForeColor = System.Drawing.Color.Navy;
-            this.button1.Location = new System.Drawing.Point(71, 183);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(350, 81);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Ускоренный";
-            this.button1.UseVisualStyleBackColor = false;
-            // 
-            // button2
-            // 
-            this.button2.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button2.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Navy;
-            this.button2.FlatAppearance.BorderSize = 2;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Rubik Mono One", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button2.ForeColor = System.Drawing.Color.Navy;
-            this.button2.Location = new System.Drawing.Point(71, 34);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(350, 81);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Классический";
-            this.button2.UseVisualStyleBackColor = false;
-            // 
-            // button3
-            // 
-            this.button3.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.button3.BackColor = System.Drawing.Color.CornflowerBlue;
-            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.Navy;
-            this.button3.FlatAppearance.BorderSize = 2;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Font = new System.Drawing.Font("Rubik Mono One", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button3.ForeColor = System.Drawing.Color.Navy;
-            this.button3.Location = new System.Drawing.Point(71, 333);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(350, 81);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Своя игра";
-            this.button3.UseVisualStyleBackColor = false;
-            // 
-            // label2
-            // 
-            this.label2.Font = new System.Drawing.Font("Rubik Mono One", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.ForeColor = System.Drawing.Color.Navy;
-            this.label2.Location = new System.Drawing.Point(495, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(486, 149);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Стандартные правила игры, пошаговые выстрелы. Поле 10х10";
-            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.ForeColor = System.Drawing.Color.Navy;
+            this.label4.Location = new System.Drawing.Point(495, 224);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(486, 112);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Настройка флота и расстановки кораблей вручную на поле 10x10";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // label3
             // 
-            this.label3.Font = new System.Drawing.Font("Rubik Mono One", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.label3.ForeColor = System.Drawing.Color.Navy;
-            this.label3.Location = new System.Drawing.Point(495, 149);
+            this.label3.Location = new System.Drawing.Point(495, 112);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(484, 149);
+            this.label3.Size = new System.Drawing.Size(484, 112);
             this.label3.TabIndex = 4;
             this.label3.Text = "Ускоренная классическая версия. Поле 8х8";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // label4
+            // CreateButton
             // 
-            this.label4.Font = new System.Drawing.Font("Rubik Mono One", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.ForeColor = System.Drawing.Color.Navy;
-            this.label4.Location = new System.Drawing.Point(495, 298);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(486, 149);
-            this.label4.TabIndex = 5;
-            this.label4.Text = "Настройка флота и расстановки кораблей вручную на поле 10x10";
-            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.CreateButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.CreateButton.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.CreateButton.FlatAppearance.BorderColor = System.Drawing.Color.Navy;
+            this.CreateButton.FlatAppearance.BorderSize = 2;
+            this.CreateButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CreateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CreateButton.ForeColor = System.Drawing.Color.Navy;
+            this.CreateButton.Location = new System.Drawing.Point(71, 239);
+            this.CreateButton.Name = "CreateButton";
+            this.CreateButton.Size = new System.Drawing.Size(350, 81);
+            this.CreateButton.TabIndex = 2;
+            this.CreateButton.Text = "Своя игра";
+            this.CreateButton.UseVisualStyleBackColor = false;
+            // 
+            // FastButton
+            // 
+            this.FastButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.FastButton.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.FastButton.FlatAppearance.BorderColor = System.Drawing.Color.Navy;
+            this.FastButton.FlatAppearance.BorderSize = 2;
+            this.FastButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.FastButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FastButton.ForeColor = System.Drawing.Color.Navy;
+            this.FastButton.Location = new System.Drawing.Point(71, 127);
+            this.FastButton.Name = "FastButton";
+            this.FastButton.Size = new System.Drawing.Size(350, 81);
+            this.FastButton.TabIndex = 0;
+            this.FastButton.Text = "Ускоренный";
+            this.FastButton.UseVisualStyleBackColor = false;
+            // 
+            // ClassicButton
+            // 
+            this.ClassicButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.ClassicButton.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.ClassicButton.FlatAppearance.BorderColor = System.Drawing.Color.Navy;
+            this.ClassicButton.FlatAppearance.BorderSize = 2;
+            this.ClassicButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ClassicButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ClassicButton.ForeColor = System.Drawing.Color.Navy;
+            this.ClassicButton.Location = new System.Drawing.Point(71, 15);
+            this.ClassicButton.Name = "ClassicButton";
+            this.ClassicButton.Size = new System.Drawing.Size(350, 81);
+            this.ClassicButton.TabIndex = 1;
+            this.ClassicButton.Text = "Классический";
+            this.ClassicButton.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.Navy;
+            this.label2.Location = new System.Drawing.Point(495, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(486, 112);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Стандартные правила игры, пошаговые выстрелы. Поле 10х10";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // BackButton
+            // 
+            this.BackButton.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.BackButton.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.BackButton.FlatAppearance.BorderColor = System.Drawing.Color.Navy;
+            this.BackButton.FlatAppearance.BorderSize = 2;
+            this.BackButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BackButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BackButton.ForeColor = System.Drawing.Color.Navy;
+            this.BackButton.Location = new System.Drawing.Point(71, 352);
+            this.BackButton.Name = "BackButton";
+            this.BackButton.Size = new System.Drawing.Size(350, 81);
+            this.BackButton.TabIndex = 6;
+            this.BackButton.Text = "Назад";
+            this.BackButton.UseVisualStyleBackColor = false;
+            this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
             // 
             // ModeForm
             // 
@@ -162,6 +182,7 @@ namespace BattleShip_WPF
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(162)))), ((int)(((byte)(191)))), ((int)(((byte)(244)))));
             this.ClientSize = new System.Drawing.Size(984, 561);
+            this.ControlBox = false;
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.label1);
             this.MaximizeBox = false;
@@ -177,11 +198,12 @@ namespace BattleShip_WPF
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button FastButton;
+        private System.Windows.Forms.Button CreateButton;
+        private System.Windows.Forms.Button ClassicButton;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button BackButton;
     }
 }
